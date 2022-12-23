adf8017dfd20a567a947e22142b9a09a0e5e773c
LenaMaistrenko <istlauder@gmail.com>
2022-12-23 17:50:20 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,5 @@
 const listCategories = document.querySelectorAll(".js-item");
 console.log(`Number of categories: ${listCategories.length}`);
-console.log(" ");
 listCategories.forEach((listItem) => {
   console.log(`Category: ${listItem.firstElementChild.textContent}`);
   console.log(`Elements: ${listItem.lastElementChild.children.length}`);
